2bd5f75152ca61238c1b9a0b97122b713df26fd9
Alex Hernandez <porialex@gmail.com>
2018-06-02 14:53:46 -0600

    🔧 vuepress meta

---
+++ website/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "Alex Hernandez | Product Designer & Front-End Developer",
+  description: "Something, something darkside.",
+  head: [
+    ["link", { type: "image/x-icon", rel: "icon", href: "/favicon.ico" }],
+    [
+      "link",
+      {
+        type: "text/css",
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Libre+Baskerville|Libre+Franklin"
+      }
+    ]
+  ],
+  markdown: {
+    anchor: {
+      permalink: false
+    }
+  }
+};
