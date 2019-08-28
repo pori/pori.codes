20f20e61b324d94e1dddd0145dd57b39261931c2
Alex Hernandez <porialex@gmail.com>
2019-08-28 18:22:01 -0400

    💄get avatar from GitHub

--- website/.vuepress/theme/Hero.vue
+++ website/.vuepress/theme/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hero">
-    <img class="avatar" src="/me.jpg" alt="Alex Hernandez">
+    <img class="avatar" src="//github.com/pori.png" alt="Alex Hernandez" />
   </div>
 </template>
 
@@ -16,6 +16,17 @@
   width: 230px;
   border-radius: 50%;
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.5);
+  animation: avatar-fadein 1s linear;
+}
+
+@keyframes avatar-fadein {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
 }
 
 @media (max-width: 1024px) {
